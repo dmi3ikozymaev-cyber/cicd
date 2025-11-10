e60e4f53c8dcb0217133aee6cbdcb14b1c15ddca
Dmitry Kozymaev <kozymaev_de@drom.ru>
2025-11-11 01:41:48 +0700

    start

---
+++ src/test/java/tests/MainPageTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainPageTest extends BaseTest {
+
+  @Test
+  public void checkAutoButtonText() {
+    driver.get("https://auto.drom.ru");
+    WebElement button = driver.findElement(By.cssSelector("a[href='https://auto.drom.ru/']"));
+    String buttonText = button.getText();
+    Assert.assertEquals(buttonText, "Авто");
+  }
+
+  @Test
+  public void checkSpectButtonText() {
+    driver.get("https://auto.drom.ru");
+    WebElement button = driver.findElement(By.cssSelector("a[href='https://auto.drom.ru/spec/']"));
+    String buttonText = button.getText();
+    Assert.assertEquals(buttonText, "Авто");
+  }
+}
